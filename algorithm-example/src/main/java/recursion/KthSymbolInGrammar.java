95fe261b336277dcc0336a6c443b8f7d8147446a
Andy-Gong <gongxch@163.com>
2020-05-13 00:20:27 +0800

    add kth symbollngrammer example

---
+++ algorithm-example/src/main/java/recursion/KthSymbolInGrammar.java
@@ -0,0 +1,74 @@
+package recursion;
+
+import common.ListNode;
+
+/**
+ * On the first row, we write a 0. Now in every subsequent row, we look at the previous row and replace each occurrence of 0 with 01, and each
+ * occurrence of 1 with 10. Given row N and index K, return the K-th indexed symbol in row N. (The values of K are 1-indexed.) (1 indexed).
+ *
+ * Examples:
+ * Input: N = 1, K = 1
+ * Output: 0
+ *
+ * Input: N = 2, K = 1
+ * Output: 0
+ *
+ * Input: N = 2, K = 2
+ * Output: 1
+ *
+ * Input: N = 4, K = 5
+ * Output: 1
+ *
+ * Explanation:
+ * row 1: 0
+ * row 2: 01
+ * row 3: 0110
+ * row 4: 01101001
+ *
+ * source：LeetCode
+ * link：https://leetcode-cn.com/problems/k-th-symbol-in-grammar
+ */
+public class KthSymbolInGrammar {
+
+    public int kthGrammar(int N, int K) {
+        if (N == 1) {
+            return 0;
+        }
+        ListNode listNode = new ListNode(0);
+        return kthGrammar(N, 2, listNode, K);
+    }
+
+    public int kthGrammar(int N, int index, ListNode value, int K) {
+        if (index == N + 1) {
+            for (int i = 0; i < K; i++) {
+                value = value.next;
+            }
+            return value.val;
+
+        }
+        ListNode newValue = null;
+        while (value != null) {
+            if (newValue == null) {
+                newValue = new ListNode(0);
+                newValue.next = new ListNode(1);
+            } else {
+                if (newValue.val == 0) {
+                    newValue.next = new ListNode(0);
+                    newValue.next.next = new ListNode(1);
+                } else {
+                    newValue.next = new ListNode(1);
+                    newValue.next.next = new ListNode(0);
+                }
+
+            }
+            value = value.next;
+        }
+        return kthGrammar(N++, index, newValue, K);
+
+    }
+
+    public static void main(String[] args) {
+        KthSymbolInGrammar kthSymbolInGrammar = new KthSymbolInGrammar();
+        kthSymbolInGrammar.kthGrammar(2, 1);
+    }
+}
